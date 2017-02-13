616ae6909c36631c59ed4222fa08d68eb1e2983b
gstrand_whb <gstrand_whb@WHB428>
2017-02-13 10:06:54 -0600

    Add project files.

---
+++ HelloWPF/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWPF
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+            var emp = new Employee()
+            {
+                Name = "Tom",
+                Title = "Developer"
+            };
+            return emp;
+        }
+    }
+}
